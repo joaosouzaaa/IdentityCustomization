690d0dc732b0030ffffb2768f9257468b3757645
joaosouzaaa <joaoasouza982@gmail.com>
2024-10-19 17:35:44 -0300

    Feat: Adding initial API project.

---
+++ src/IdentityCustomization.API/DependencyInjection/DependencyInjectionHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityCustomization.API.DependencyInjection;
+
+internal static class DependencyInjectionHandler
+{
+    internal static void AddDependencyInjection(this IServiceCollection services)
+    {
+
+    }
+}
